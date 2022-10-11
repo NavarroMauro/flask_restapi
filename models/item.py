116505d7a9bcb4d733e07e1b86d4c5115bc2898f
Mauricio Navarro <mcifuent@gmail.com>
2022-10-11 11:07:45 +0200

    ready

--- models/item.py
+++ models/item.py
@@ -1,13 +1,12 @@
 from db import db
 
-
 class ItemModel(db.Model):
     __tablename__ = "items"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
     price = db.Column(db.Float(precision=2), unique=False, nullable=False)
-
+    description = db.Column(db.String, unique=False)
     store_id = db.Column(
         db.Integer, db.ForeignKey("stores.id"), unique=False, nullable=False
     )
